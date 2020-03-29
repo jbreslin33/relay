9f9f685d7f43a5c7dee9c5d48802af6ac4f46fb7
Jim Breslin <jbreslin33@gmail.com>
2020-03-28 21:31:21 -0400

    unneeded

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-
 #include <iostream>       // std::cout
 #include <thread>         // std::thread, std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
 
 #include "relay.h"         // std::chrono::seconds
 
