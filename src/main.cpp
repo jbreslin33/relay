c804b1bf7c75508d65fe55185ff441bb51c8b8fc
Jim Breslin <jbreslin33@gmail.com>
2020-03-31 20:33:48 -0400

    round trip works

--- src/main.cpp
+++ src/main.cpp
@@ -85,17 +85,50 @@ void writeData(Relay* relay)
     		}
   	}
 }
-/*
 void readSocketData(Relay* relay)
 {
+	int sock;
+  	struct sockaddr_in sa;
+  	char buffer[1024];
+  	ssize_t recsize;
+  	socklen_t fromlen;
+
+  	memset(&sa, 0, sizeof sa);
+  	sa.sin_family = AF_INET;
+  	sa.sin_addr.s_addr = htonl(INADDR_ANY);
+  	sa.sin_port = htons(8765);
+  	fromlen = sizeof sa;
+
+  	sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  	if (bind(sock, (struct sockaddr *)&sa, sizeof sa) == -1) 
+	{
+    		perror("error bind failed");
+    		close(sock);
+    		exit(EXIT_FAILURE);
+  	}
+
+  	for (;;) 
+	{
+    		recsize = recvfrom(sock, (void*)buffer, sizeof buffer, 0, (struct sockaddr*)&sa, &fromlen);
+    		if (recsize < 0) 
+		{
+      			fprintf(stderr, "%s\n", strerror(errno));
+      			exit(EXIT_FAILURE);
+   		}
+		relay->log(buffer);
+    		printf("recsize: %d\n ", (int)recsize);
+    		sleep(1);
+    		printf("datagram: %.*s\n", (int)recsize, buffer);
+  	}
+  /*
 	while (true) 
 	{
 		//relay->mListenSocket->readData();
 		//relay->readData();
 		//relay->log("read Data");
 	}
+	*/
 }
-*/
 
 int main(void)
 {
@@ -103,10 +136,10 @@ int main(void)
 
 	std::thread reader       (readData, &relay);     
 	std::thread writer       (writeData, &relay);     
-	//std::thread socketReader (readSocketData, &relay);     
+	std::thread socketReader (readSocketData, &relay);     
 	reader.join();
 	writer.join();
-	//socketReader.join();
+	socketReader.join();
 
   	return 0;
 }
